2d1314ccec058c0f236e877f5626074ab5348bbe
Jill <zvjil@hotmail.com>
2020-06-23 12:03:55 -0400

    added header and  nav

--- ecomm/src/components/layout.js
+++ ecomm/src/components/layout.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
+import Header from './headernav/header';
+import Navbar from './headernav/navbar';
 
 class Layout extends Component {
   render () {
     return (
-      <div className="app">
+      <div className="layout">
 
         {this.props.children}
+        <Header />
+        <Navbar />
       </div>
     );
   }
